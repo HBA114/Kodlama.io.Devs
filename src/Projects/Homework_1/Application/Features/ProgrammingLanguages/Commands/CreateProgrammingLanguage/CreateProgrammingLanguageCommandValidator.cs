87e86af3e2c4ec0c1438a82e41fab9cd9b00443f
HBA114 <hbasriayhaner114@gmail.com>
2022-09-03 01:17:18 +0300

    Added ProgrammingLanguage Name Validator with Fluent Validation

---
+++ src/Projects/Homework_1/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+    {
+        public CreateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+        }
+    }
+}
