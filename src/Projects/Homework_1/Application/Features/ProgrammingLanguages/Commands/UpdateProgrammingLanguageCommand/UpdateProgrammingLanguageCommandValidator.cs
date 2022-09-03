3cf64d88bfbe7c187e7fc7cfd4febe2d4175f0a2
HBA114 <hbasriayhaner114@gmail.com>
2022-09-03 23:07:17 +0300

    Added validator for Update Command

---
+++ src/Projects/Homework_1/Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguageCommand/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguageCommand
+{
+    public class UpdateProgrammingLanguageCommandValidator : AbstractValidator<UpdateProgrammingLanguageCommand>
+    {
+        public UpdateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+        }
+    }
+}
