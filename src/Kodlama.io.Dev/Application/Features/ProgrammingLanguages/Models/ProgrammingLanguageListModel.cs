ced83ca82131e24bf0ee3b605d7e444f6edccadc
HBA114 <hbasriayhaner114@gmail.com>
2022-09-11 15:11:25 +0300

    ProgrammingLanguage ListModel inherited from BasePageableModel

--- src/Kodlama.io.Dev/Application/Features/ProgrammingLanguages/Models/ProgrammingLanguageListModel.cs
+++ src/Kodlama.io.Dev/Application/Features/ProgrammingLanguages/Models/ProgrammingLanguageListModel.cs
@@ -1,13 +1,9 @@
 ﻿using Application.Features.ProgrammingLanguages.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Core.Persistence.Paging;
 
 namespace Application.Features.ProgrammingLanguages.Models
 {
-    public class ProgrammingLanguageListModel
+    public class ProgrammingLanguageListModel : BasePageableModel
     {
         public IList<ProgrammingLanguageListDto> Items { get; set; }
     }
